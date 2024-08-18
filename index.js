22f25566f68b45160ebb76f986c12a58120b021f
Mazid70 <mazidurmazid431@gmail.com>
2024-08-18 15:06:20 +0600

    link updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(
     origin: [
       'http://localhost:5174',
       'http://localhost:5173',
-      'https://assettrackerpro.netlify.app',
+      'https://shadhinmart.netlify.app',
     ],
     credentials: true,
   })
